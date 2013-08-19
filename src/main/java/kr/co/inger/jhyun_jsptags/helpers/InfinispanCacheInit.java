5500d3f4630a08985f12ba383751e096dd2ac0cf
jonghyouk yun <ageldama@gmail.com>
2013-08-19 19:18:09 +0900

    javax.cache upgraded.

--- src/main/java/kr/co/inger/jhyun_jsptags/helpers/InfinispanCacheInit.java
+++ src/main/java/kr/co/inger/jhyun_jsptags/helpers/InfinispanCacheInit.java
@@ -1,9 +1,9 @@
 package kr.co.inger.jhyun_jsptags.helpers;
 
-import javax.cache.Cache;
 import javax.cache.CacheManager;
 import javax.cache.Caching;
-import javax.cache.MutableConfiguration;
+import javax.cache.configuration.Configuration;
+import javax.cache.configuration.MutableConfiguration;
 import javax.cache.spi.CachingProvider;
 
 import kr.co.inger.jhyun_jsptags.CachedTag;
@@ -13,8 +13,8 @@ public class InfinispanCacheInit {
 	public static void doInit() {
 		CachingProvider cp = Caching.getCachingProvider();
 		CacheManager cm = cp.getCacheManager();
-		Cache c1 = cm.configureCache(CachedTag.CACHE_NAME,
-				new MutableConfiguration<Object, Object>());
+		Configuration cfg = new MutableConfiguration<Object, Object>();
+		cm.createCache(CachedTag.CACHE_NAME, cfg);
 	}
 
 }
